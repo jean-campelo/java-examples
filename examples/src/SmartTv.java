363b528d5ea69de42021ab1ac024c8d7033fde30
Jean Campelo <jean93campelo@gmail.com>
2023-11-24 16:38:54 -0300

    user actions

--- examples/src/SmartTv.java
+++ examples/src/SmartTv.java
@@ -2,4 +2,28 @@ public class SmartTv {
     boolean activated = false;
     int channel = 1;
     int soundVolum = 10;
+
+    public void changeStatusEnergy() {
+        activated = !activated;
+    }
+
+    public void upSound() {
+        soundVolum++;
+    }
+
+    public void downSound() {
+        soundVolum--;
+    }
+
+    public void upChannel() {
+        channel++;
+    }
+
+    public void downChannel() {
+        channel--;
+    }
+
+    public void changeChannel(int num) {
+        channel = num;
+    }
 }
--- examples/src/User.java
+++ examples/src/User.java
@@ -4,7 +4,26 @@ public class User {
 
         System.out.println("Tv is on? " + smartTv.activated);
         System.out.println("The current channel: " + smartTv.channel);
+        System.out.println("The sound is: " + smartTv.soundVolum + "\n");
+
+        System.out.println("====== User actions ======");
+
+        smartTv.changeStatusEnergy();
+        System.out.println("Tv is on? " + smartTv.activated);
+
+        smartTv.upSound();
+        smartTv.upSound();
+        smartTv.upSound();
+        smartTv.downSound();
         System.out.println("The sound is: " + smartTv.soundVolum);
+
+        smartTv.upChannel();
+        System.out.println("The current channel: " + smartTv.channel);
+        smartTv.downChannel();
+        System.out.println("The current channel: " + smartTv.channel);
+
+        smartTv.changeChannel(50);
+        System.out.println("The current channel: " + smartTv.channel);
 
     }
 }
