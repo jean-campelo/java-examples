77aa0203831a2033725c2b17c87c32e336b77d72
Jean Campelo <jean93campelo@gmail.com>
2023-11-29 11:19:48 -0300

    Add method documentation

---
+++ examples/src/Calculator.java
@@ -0,0 +1,21 @@
+/**
+ * @autor Jean Campelo
+ * @version 1.0
+ * @since 2023.11.29
+ */
+
+public class Calculator {
+    /**
+     * Method for adding two integer values
+     * 
+     * @params num1 first value
+     * 
+     * @params num2 second value
+     * 
+     * @return int result of the sum between num1 and num2
+     */
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
